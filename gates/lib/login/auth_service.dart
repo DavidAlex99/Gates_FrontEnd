ed38367cf9bcb7fa5f054c62f4a227e20317c5ad
DavidAlex99 <daalrive@espol.edu.ec>
2024-03-17 11:07:47 -0500

    creacion de usuario y cliente con token y sincronizacion con la bbdd

---
+++ gates/lib/login/auth_service.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  final String baseUrl =
+      'http://192.168.100.6:8001/gatesApp'; // Reemplaza esto por la URL real de tu backen
+
+  // Método para guardar el token
+  Future<void> _saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_token', token);
+  }
+
+  Future<String?> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/api-token-auth/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      String token = responseData['token'];
+      await _saveToken(token); // Guardar el token
+      return token;
+    } else {
+      return null;
+    }
+  }
+
+  Future<String?> register(
+      String username, String email, String password, String nombre) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/register/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'email': email,
+        'password': password,
+        'nombre': nombre,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      final responseData = jsonDecode(response.body);
+      String token = responseData['token'];
+      await _saveToken(token); // Guardar el token
+      return token;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ gates/lib/login/signup_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'auth_service.dart';
+import 'session_manager.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nombreController =
+      TextEditingController(); // Campo adicional para el teléfono
+
+  void _register() async {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    String nombre = _nombreController.text; // Recolectar el valor del teléfono
+    String? userId =
+        await AuthService().register(username, email, password, nombre);
+    if (userId != null) {
+      // pasar el userId del usuario actual
+      /*Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => EmprendimientosPage(userId: userId)
+        ),
+      );*/
+    } else {
+      // Mostrar error si el registro falla
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Error de Registro"),
+            content: Text(
+                "No se pudo completar el registro. Por favor, inténtalo de nuevo."),
+            actions: <Widget>[
+              TextButton(
+                child: Text("Cerrar"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Registro")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            TextField(
+              controller: _nombreController,
+              decoration: InputDecoration(labelText: 'Nombre'),
+            ),
+            ElevatedButton(
+              onPressed: _register,
+              child: Text('Registrarse'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
